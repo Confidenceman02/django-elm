d664521ee21e8bce5c653da709cc733aad69531e
Confidenceman02 <25443812+Confidenceman02@users.noreply.github.com>
2026-06-02 04:44:07 +0930

    Bump transformer version (#187)
    Supports compatible parcel version and limits scope.
    
    Co-authored-by: Jaime Terreu <>

--- packages/transformer-elm/lib/ElmTransformer.js
+++ packages/transformer-elm/lib/ElmTransformer.js
@@ -142,7 +142,7 @@ function formatElmError(problem, relativePath) {
     ].join("\n");
     return {
         message,
-        origin: "@confidenceman/parcel-transformer-djelm",
+        origin: "@confidenceman02/parcel-transformer-djelm",
         stack: "", // set stack to empty since it is not useful
     };
 }
